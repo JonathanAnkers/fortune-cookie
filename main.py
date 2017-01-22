afbd6a9f4c5572842a96385711135454d7d4981c
JonathanAnkers <jonathan.ankers@gmail.com>
2017-01-22 14:47:47 -0500

    final commit

--- main.py
+++ main.py
@@ -17,14 +17,47 @@
 import webapp2
 import random
 
+def getRandomFortune():
+	
+	fortunes = ["The world is yours", "You will be a great coder!","You will be in control of your own Fortune"]
+	index = random.randint(0,2)
+
+	return fortunes[index]
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
     	header = "<h1>Frotune Cookie</h1>"
-    	lucky_number = random.randrange(100)
-    	number_sent = '<p>Your lucky number: ' + str(lucky_number) + '</p>' 
-        self.response.write(header + number_sent)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    	fortune = "<strong>" + getRandomFortune() + "</strong>"
+    	fortune_sent = "Your Fortune: " + fortune
+    	fortune_paragraph = "<p>" + fortune_sent + "</p>"
+    	
+
+    	lucky_number = "<strong>" + str(random.randrange(100)) + "</strong>"
+    	number_sent = '<p>Your lucky number: ' + lucky_number + '</p>' 
+        
+        cookie_again_button = "<a href='.'><button>Another cookie please</button></a>"
+
+        content = header + fortune_paragraph + number_sent + cookie_again_button
+
+
+
+        self.response.write(content)
+        
+
+
+        self.response.write('<a href="localhost:8080/login"><button>Login</button></a>')
+
+
+class LoginHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.write('Love is the best')
+
+routes = [
+	('/', MainHandler),
+	('/login', LoginHandler)
+]
+app = webapp2.WSGIApplication(routes, debug=True)
+
